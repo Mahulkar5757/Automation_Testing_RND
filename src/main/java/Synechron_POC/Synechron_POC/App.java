c474047be89c4e89c67cee39c292f94e37bcfec4
Devendra.Mahulkar <Devendra.Mahulkar@SYPULJ53WZ93.synechron.com>
2021-05-10 12:55:04 +0530

    Testing

---
+++ src/main/java/Synechron_POC/Synechron_POC/App.java
@@ -0,0 +1,13 @@
+package Synechron_POC.Synechron_POC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
