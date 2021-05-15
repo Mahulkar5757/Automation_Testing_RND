46e893c9557b4fe0c8aa7e574dbaaba368dfe665
Devendra.Mahulkar <Devendra.Mahulkar@SYPULJ53WZ93.synechron.com>
2021-05-15 17:00:29 +0530

    Final Push

---
+++ src/test/java/demo/Poc/Pages/HomePage.java
@@ -0,0 +1,31 @@
+package demo.Poc.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+	
+	WebDriver ldriver;
+	
+	public HomePage(WebDriver rdriver)
+	{
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+		
+	@FindBy(xpath="//body[1]/nav[1]/ul[1]/li[3]/a[1]/i[1]")
+	@CacheLookup
+	WebElement btnSyneTime;
+	
+	
+	
+	public void clickSyneTime()
+	{
+		btnSyneTime.click();
+	}
+	
+	
+}
--- src/test/java/demo/Poc/Testcases/TC_LoginTest_001.java
+++ src/test/java/demo/Poc/Testcases/TC_LoginTest_001.java
@@ -3,9 +3,11 @@ package demo.Poc.Testcases;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import demo.Poc.Pages.HomePage;
 import demo.Poc.Pages.LoginPage;
 
 
@@ -13,12 +15,13 @@ public class TC_LoginTest_001 extends BaseClass
 {
 
 	@Test
-	public void loginTest() throws IOException 
+	public void loginTest() throws IOException, InterruptedException 
 	{
 			
 		logger.info("URL is opened");
 		
 		LoginPage lp=new LoginPage(driver);
+		HomePage hp=new HomePage(driver);
 		lp.setUserName(username);
 		logger.info("Entered username");
 		
@@ -27,7 +30,15 @@ public class TC_LoginTest_001 extends BaseClass
 		
 		lp.clickSubmit();
 		
-		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+		hp.clickSyneTime();
+		
+	
+		
+		driver.findElement(By.xpath("attendence_popup_Ind")).click();;
+		
+		Thread.sleep(7000);
+		
+		if(driver.getTitle().equals("Testing"))
 		{
 			Assert.assertTrue(true);
 			logger.info("Login test passed");
